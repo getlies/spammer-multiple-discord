cb440cf30e99fb5f7b6ae3b7e1d3a9c0519a2fa6
getlies <9ball0005@gmail.com>
2025-06-13 04:56:17 +0700

    Remove autowakeup functionality: delete autoWakeup.js and clean up handleCommands.js

--- src/handleCommands.js
+++ src/handleCommands.js
@@ -1,6 +1,5 @@
 const config = require("../setup/config.js");
 const say = require("./commands/say.js"); 
-const autoWakeup = require("./utils/autoWakeup.js"); 
 const spamControl = require("./utils/spamControl");
 
 function handleCommands(client, clients) {
@@ -10,8 +9,6 @@ function handleCommands(client, clients) {
 
     client.on('messageCreate', message => {
         const isAllowed = jockieIds && jockieIds.includes(message.author.id);
-
-    autoWakeup(client, message); 
 
         if (message.content.startsWith(`${prefix}say `)) {
             const args = message.content.split(' ').slice(1);
